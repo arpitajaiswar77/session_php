6db696cd3875d9b2d366b399e4e996b2cb2071fe
arpitajaiswar77 <jaiswararpita.it@gmail.com>
2026-03-31 10:23:04 +0530

    intial commit

---
+++ add.php
@@ -0,0 +1,113 @@
+<?php
+include 'db.php';
+if ($_SERVER["REQUEST_METHOD"]==="POST") {
+    $name=$_POST["name"];
+    $qty=$_POST["qty"];
+    $price=$_POST["price"];
+
+    $sql=$conn->prepare("insert into product values(?,?,?)");
+    $sql->bind_param("sid"  ,$name,$qty,$price);
+
+    if ($sql->execute()) {
+        echo "<script>alert('Inserted')</script>";
+        header("Location:home.php");
+    }
+}
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main>
+            <div
+                class="container col-5 p-5 rounded shadow border mt-5 "
+            >
+                <form action="" method="POST">
+                    <div class="mb-3">
+                <label for="" class="form-label">Product name</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="name"
+                    id=""
+                    aria-describedby="helpId"
+                    placeholder=""
+                />
+               
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Product Qty</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="qty"
+                    id=""
+                    aria-describedby="helpId"
+                    placeholder=""
+                />
+               
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Product price</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="price"
+                    id=""
+                    aria-describedby="helpId"
+                    placeholder=""
+                />
+              
+            </div>
+            <button
+                type="submit"
+                class="btn btn-primary"
+            >
+               Submit
+            </button>
+            
+                </form>
+            </div>
+            
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
---
+++ home.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+include "db.php";
+
+
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+           <nav
+            class="navbar navbar-expand-sm navbar-light bg-light"
+           >
+            <div class="container">
+               
+            <h5>Hello <?php echo $_SESSION["name"] ?></h5>
+                <button
+                    class="navbar-toggler d-lg-none"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavId"
+                    aria-controls="collapsibleNavId"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="add.php" aria-current="page"
+                                >Add product
+                                <span class="visually-hidden">(current)</span></a
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="update.php">Update Product</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="delete.php">delete Product</a>
+                        </li>
+                    </ul>
+                    <form action="logout.php" method="post">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+        Logout
+    </button>
+</form>
+                    </form>
+                </div>
+            </div>
+           </nav>
+           
+        </header>
+        <main><?php
+include 'db.php';
+$result=$conn->query("select * from product");
+?>
+<h1 ><center>PRODUCTS</center></h1>
+
+    <table cellpadding="25"cellspacing="6" border="2" style="margin:auto;background:linear-gradient(lightblue,lightpink,lightgreen)";>
+        <tr>
+            <th>Product Name</th>
+            <th>Product Qty</th>
+            <th>Product Price</th>
+        </tr>
+        <?php while ($row=$result->fetch_assoc()) {?>
+            <tr>
+                <td><?php echo $row["name"];?></td>
+                <td><?php echo $row["qty"];?></td>
+                <td><?php echo $row["price"];?></td>
+            </tr>
+        <?php } ?>
+
+    </table>
+</main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
